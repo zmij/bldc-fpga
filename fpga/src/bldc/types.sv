3d2cf538c91c4f38a0a4fe80adce21da806a2577
Sergei Fedorov <sergei.a.fedorov@gmail.com>
2023-08-02 17:11:14 +0200

    BLDC table commutator
    * Add table BLCD commutation module
    * Add the module to APB peripheral (currently uses detected
      direction for commutation)
    * Add phase enable field to status register
    * Add handing code to C++

--- fpga/src/bldc/types.sv
+++ fpga/src/bldc/types.sv
@@ -17,5 +17,16 @@ typedef enum logic [1:0] {
   DIR_BRAKE = 2'b10
 } rotation_direction_t;
 
+typedef enum bit [2:0] {
+  PHASE_OFF = 3'b000,
+  PHASE_A_C = 3'b101,
+  PHASE_A   = 3'b100,
+  PHASE_A_B = 3'b110,
+  PHASE_B   = 3'b010,
+  PHASE_B_C = 3'b011,
+  PHASE_C   = 3'b001,
+  PHASE_ON  = 3'b111
+} phase_states_t;
+
 
 `endif  // __BLDC_TYPES_SV__
